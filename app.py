cd9ae05a5706f0331cb7eab211de13bc1f7d1ef2
DanG <minigomezdd@gmail.com>
2025-10-16 16:52:06 -0600

    Primer commit - 21590082

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app =  Flask(__name__)
 #Ruta raiz
 @app.route('/')
 def index():
-    return 'Hola Mundo, esto es una prueba con contenedores automaticos, CADA 1 MINUTO'
+    return 'Hola Mundo, esto es una prueba con contenedores automaticos (CADA 1 MINUTO)'
 
 if __name__ == '__main__':
     app.run(debug=True)
